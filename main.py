4193a99bfa6259b61523cfba62b357392e0d0817
TMUngetti <tommaso.ungetti@gmail.com>
2023-12-30 18:31:49 +0100

    minor fixes and added progressive scraping

--- main.py
+++ main.py
@@ -65,17 +65,15 @@ def scrape(web_page):
 def logo():
     print(Fore.RED + '''
 
-       d8888 888    888                                
-      d88888 888    888                                
-     d88P888 888    888                                
-    d88P 888 888888 88888b.   .d88b.  88888b.   8888b. 
-   d88P  888 888    888 "88b d8P  Y8b 888 "88b     "88b
-  d88P   888 888    888  888 88888888 888  888 .d888888
- d8888888888 Y88b.  888  888 Y8b.     888  888 888  888
-d88P     888  "Y888 888  888  "Y8888  888  888 "Y888888
-
+ █████╗ ████████╗██╗  ██╗███████╗███╗   ██╗ █████╗ 
+██╔══██╗╚══██╔══╝██║  ██║██╔════╝████╗  ██║██╔══██╗
+███████║   ██║   ███████║█████╗  ██╔██╗ ██║███████║
+██╔══██║   ██║   ██╔══██║██╔══╝  ██║╚██╗██║██╔══██║
+██║  ██║   ██║   ██║  ██║███████╗██║ ╚████║██║  ██║
+╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝╚══════╝╚═╝  ╚═══╝╚═╝  ╚═╝
+                                                   
     ''')
-    print(Fore.WHITE + 'Spanish News facility by Tommaso Maria Ungetti\nReleased under GPL Licence - v0.1\n\n')
+    print(Fore.WHITE + 'Spanish News facility by Tommaso Maria Ungetti\nReleased under GPL Licence - v1.0\n\n')
 
 # MAIN LOGIC
 
@@ -86,14 +84,18 @@ logo()
 opt = starting_menu(OPTIONS)
 
 if opt == 'El Pais - Actualidad':
+    clear()
+    logo()
     while True:
         articles = scrape(WEB_PAGE)
         clear()
+        logo()
         for i in range(len(articles[0])):
             print(i + 1)
             print(Fore.RED + "[Headline]", Fore.WHITE + f": {articles[0][i]}")
             print(Fore.RED + "[URL]", Fore.WHITE + f": {articles[1][i]}")
             print(Fore.RED + "[Date]", Fore.WHITE + f": {articles[2][i]}\n\n")
+            input()
         flag = input("\nRefresh? (y/n): ")
 
         if flag == "y":
